07ab61e72ca0b51cb0800dcf8c890d24a84eb575
Tanu Panwar <tanupanwar1098@gmail.com>
2026-06-12 16:58:10 +0530

    Add files via upload

---
+++ Task2_Simple_Calculator/calculator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char op;
+
+    // Input numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Input operation
+    cout << "Choose operation (+, -, *, /): ";
+    cin >> op;
+
+    // Perform calculation
+    switch(op) {
+        case '+':
+            cout << "Result = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << "Result = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << "Result = " << num1 * num2;
+            break;
+
+        case '/':
+            if (num2 != 0)
+                cout << "Result = " << num1 / num2;
+            else
+                cout << "Error! Division by zero is not allowed.";
+            break;
+
+        default:
+            cout << "Invalid operation!";
+    }
+
+    return 0;
+}
