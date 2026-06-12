43d1e8607aa2d27e84a7da84c7d5ecc8fa2cf7f9
Tanu Panwar <tanupanwar1098@gmail.com>
2026-06-12 16:57:07 +0530

    Add files via upload

---
+++ Task1_Number_Guessing_Game/.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0)); // Random number generator
+
+    int secretNumber = rand() % 100 + 1; // Number between 1 and 100
+    int guess;
+    int attempts = 0;
+
+    cout << "===== Guess the Number Game =====" << endl;
+    cout << "I have chosen a number between 1 and 100." << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > secretNumber) {
+            cout << "Too high! Try again." << endl;
+        }
+        else if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        }
+        else {
+            cout << "Congratulations! You guessed the number in "
+                    << attempts << " attempts." << endl;
+        }
+
+    } while (guess != secretNumber);
+
+    return 0;
+}
